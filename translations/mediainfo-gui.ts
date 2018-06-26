31c5fddc4319625d66a5cebde2a11bd8f7620da3
arno <adekker@gmail.com>
2018-06-26 19:06:29 +0200

    translation strings added

--- translations/mediainfo-gui.ts
+++ translations/mediainfo-gui.ts
@@ -2,9 +2,59 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Mediainfo-gui is a graphical user interface for MediaInfo and ExifTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>-thanks to llelectronics for the file browser-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ExifTool not found. Using </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Using </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> and ExifTool </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>DirEntryMenu</name>
     <message>
         <source>Deleting </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DirList</name>
+    <message>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error pasting file </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File operation succeeded</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -15,18 +65,121 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>mediainfo-gui</source>
+        <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files.</source>
+        <source>Images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select media file (image/audio/video) to acquire extended tag/metadata provided by MediaInfo or ExifTool.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Includes limited Exif edit options.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MediaInfo</name>
+    <message>
+        <source>Phototime to file timestamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Setting timestamp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove GPS data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing location info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove all EXIF metadata</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing all metadata</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>OpenDialog</name>
     <message>
-        <source>Open file</source>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error pasting file </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File operation succeeded</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Using</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Preferred tool for video info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MediaInfo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ExifTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Preferred tool for audio info</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
