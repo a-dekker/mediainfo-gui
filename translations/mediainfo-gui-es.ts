400359cf6e4c636e8c0e54c22b9b1c4dae2fe1fe
Carlos   Gonzalez <carlosgonz@protonmail.com>
2018-06-26 20:13:28 -0400

    Create mediainfo-gui-es.ts

---
+++ translations/mediainfo-gui-es.ts
@@ -0,0 +1,217 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="20"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="38"/>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Mediainfo-gui is a graphical user interface for MediaInfo and ExifTool</source>
+        <translation>Mediainfo-gui es una interfaz gráfica de MediaInfo y ExifTool</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="51"/>
+        <source>-thanks to llelectronics for the file browser-</source>
+        <translation>-Gracias a llelectronics por el gestor de archivos-</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="61"/>
+        <source>ExifTool not found. Using </source>
+        <translation>No se halló ExifTool. Usando </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="63"/>
+        <source>Using </source>
+        <translation>Usando </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="65"/>
+        <source> and ExifTool </source>
+        <translation>y  ExifTool </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+</context>
+<context>
+    <name>DirEntryMenu</name>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntryMenu.qml" line="32"/>
+        <source>Deleting </source>
+        <translation>Borrando </translation>
+    </message>
+</context>
+<context>
+    <name>DirList</name>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="175"/>
+        <source>Paste</source>
+        <translation>Pegar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="231"/>
+        <source>Error pasting file </source>
+        <translation>Error al pegar archivo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="239"/>
+        <source>File operation succeeded</source>
+        <translation>Operación de archivo éxitosa</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="14"/>
+        <source>Browse</source>
+        <translation>Navegar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="19"/>
+        <source>Images</source>
+        <translation>Fotos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="24"/>
+        <source>Videos</source>
+        <translation>Videos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="59"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="63"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="110"/>
+        <source>Select media file (image/audio/video) to acquire extended tag/metadata provided by MediaInfo or ExifTool.</source>
+        <translation>Elegir el archivo multimedia (imagen/audio/video) para adquirir etiquetas/metadatos extendidos provistos por MediaInfo o ExifTool.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="123"/>
+        <source>Includes limited Exif edit options.</source>
+        <translation>Incluye opciones limitadas de edición Exif.</translation>
+    </message>
+</context>
+<context>
+    <name>MediaInfo</name>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="109"/>
+        <source>Phototime to file timestamp</source>
+        <translation>Fototiempo para registrar la marca de tiempo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="110"/>
+        <source>Setting timestamp</source>
+        <translation>Ajustes de marca de tiempo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="118"/>
+        <source>Remove GPS data</source>
+        <translation>Quitar datos GPS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="122"/>
+        <source>Removing location info</source>
+        <translation>Quitando detalles de ubicación</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="130"/>
+        <source>Remove all EXIF metadata</source>
+        <translation>Quitar todos los metadatos EXIF</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="131"/>
+        <source>Removing all metadata</source>
+        <translation>Quitando todos los metadatos</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="113"/>
+        <source>Paste</source>
+        <translation>Pegar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="144"/>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="145"/>
+        <source>Deleting </source>
+        <translation>Borrando</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="263"/>
+        <source>Cut</source>
+        <translation>Cortar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="269"/>
+        <source>Copy</source>
+        <translation>Copiar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="275"/>
+        <source>Delete</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="296"/>
+        <source>Error pasting file </source>
+        <translation>Error al pegar el archivo </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="304"/>
+        <source>File operation succeeded</source>
+        <translation>Operación de archivo éxitosa</translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="29"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="33"/>
+        <source>Video</source>
+        <translation>Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="38"/>
+        <location filename="../qml/pages/SettingPage.qml" line="65"/>
+        <source>Using</source>
+        <translation>Usando</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="39"/>
+        <source>Preferred tool for video info</source>
+        <translation>Herramienta preferida para información de video</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="60"/>
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="66"/>
+        <source>Preferred tool for audio info</source>
+        <translation>Herramienta preferida para información de audio</translation>
+    </message>
+</context>
+</TS>
