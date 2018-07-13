7b50c5b569aec299f57de425c919fa6f19e3baad
Åke Engelbrektson <eson57@users.noreply.github.com>
2018-07-13 07:37:20 +0200

    Create mediainfo-gui-sv.ts
    Swedish translation for this excellent tool

---
+++ translations/mediainfo-gui-sv.ts
@@ -0,0 +1,310 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="20"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="38"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Mediainfo-gui is a graphical user interface for MediaInfo and ExifTool</source>
+        <translation>Mediainfo-gui är ett grafiskt användargränssnitt för MediaInfo och ExifTool</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="51"/>
+        <source>-thanks to llelectronics for the file browser-</source>
+        <translation>-tack till llelectronics för filhanteraren-</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="61"/>
+        <source>ExifTool not found. Using </source>
+        <translation>ExifTool hittades inte. Använder </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="63"/>
+        <source>Using </source>
+        <translation>Använder </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="65"/>
+        <source> and ExifTool </source>
+        <translation> och ExifTool </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source>Author</source>
+        <translation>Utvecklare</translation>
+    </message>
+</context>
+<context>
+    <name>DirEntry</name>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntry.qml" line="142"/>
+        <source>directory</source>
+        <translation>mapp</translation>
+    </message>
+</context>
+<context>
+    <name>DirEntryMenu</name>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntryMenu.qml" line="32"/>
+        <source>Deleting </source>
+        <translation>Tar bort </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntryMenu.qml" line="45"/>
+        <source>Cut</source>
+        <translation>Klipp ut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntryMenu.qml" line="50"/>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirEntryMenu.qml" line="55"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>DirList</name>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="149"/>
+        <source>Show Hidden Files</source>
+        <translation>Visa dolda filer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="149"/>
+        <source>Hide Hidden Files</source>
+        <translation>Visa inte dolda filer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="153"/>
+        <source>Show Filesystem Root</source>
+        <translation>Visa filsystemets rotkatalog</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="157"/>
+        <source>Show Home</source>
+        <translation>Visa hemkatalogen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="161"/>
+        <source>Show Android SDCard</source>
+        <translation>Visa Android SD-kort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="165"/>
+        <source>Show SDCard</source>
+        <translation>Visa SD-kort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="175"/>
+        <source>Paste</source>
+        <translation>Klistra in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="191"/>
+        <source>Scroll to top</source>
+        <translation>Skrolla till början</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="231"/>
+        <source>Error pasting file </source>
+        <translation>Kunde inte klistra in filen </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/DirList.qml" line="239"/>
+        <source>File operation succeeded</source>
+        <translation>Filåtgärden lyckades</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="14"/>
+        <source>Browse</source>
+        <translation>Bläddra</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="19"/>
+        <source>Images</source>
+        <translation>Bilder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="24"/>
+        <source>Videos</source>
+        <translation>Filmer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="59"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="63"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="110"/>
+        <source>Select media file (image/audio/video) to acquire extended tag/metadata provided by MediaInfo or ExifTool.</source>
+        <translation>Välj mediefil (bild/ljud/film) för att hämta utökad tagg-/metadata från MediaInfo eller ExifTool.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="123"/>
+        <source>Includes limited Exif edit options.</source>
+        <translation>Inkluderar begränsade Exif-redigeringsalternativ.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="194"/>
+        <source>Homepage</source>
+        <translation>Hemsida</translation>
+    </message>
+</context>
+<context>
+    <name>MediaInfo</name>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="131"/>
+        <source>Phototime to file timestamp</source>
+        <translation>Fototid till filstämpel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="132"/>
+        <source>Setting timestamp</source>
+        <translation>Anger tidsstämpel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="140"/>
+        <source>Remove GPS data</source>
+        <translation>Ta bort GPS-data</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="144"/>
+        <source>Removing location info</source>
+        <translation>Tar bort platsinformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="152"/>
+        <source>Remove all EXIF metadata</source>
+        <translation>Ta bort all Exif-metadata</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MediaInfo.qml" line="153"/>
+        <source>Removing all metadata</source>
+        <translation>Tar bort all metadata</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="92"/>
+        <source>Show Filesystem Root</source>
+        <translation>Visa filsystemets rootkatalog</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="96"/>
+        <source>Show Home</source>
+        <translation>Visa hemkatalogen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="100"/>
+        <source>Show Android SDCard</source>
+        <translation>Visa Android SD-kort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="104"/>
+        <source>Show SDCard</source>
+        <translation>Visa SD-kort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="112"/>
+        <source>Paste</source>
+        <translation>Klistra in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="129"/>
+        <source>Scroll to top</source>
+        <translation>Skrolla till början</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="143"/>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="144"/>
+        <source>Deleting </source>
+        <translation>Tar bort </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="212"/>
+        <source>directory</source>
+        <translation>mapp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="262"/>
+        <source>Cut</source>
+        <translation>Klipp ut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="268"/>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="274"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="295"/>
+        <source>Error pasting file </source>
+        <translation>Kunde inte klistra in filen </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/fileman/OpenDialog.qml" line="303"/>
+        <source>File operation succeeded</source>
+        <translation>Filåtgärden lyckades</translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="29"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="33"/>
+        <source>Video</source>
+        <translation>Film</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="38"/>
+        <location filename="../qml/pages/SettingPage.qml" line="65"/>
+        <source>Using</source>
+        <translation>Använder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="39"/>
+        <source>Preferred tool for video info</source>
+        <translation>Föredraget verktyg för filminformation</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="60"/>
+        <source>Audio</source>
+        <translation>Ljud</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="66"/>
+        <source>Preferred tool for audio info</source>
+        <translation>Föredraget verktyg för Ljudinformation</translation>
+    </message>
+</context>
+</TS>
